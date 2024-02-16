618194cb6d8e4bc1b91c7a048d2bd5bd4b8a61d5
Nolan Reilly <nolanreilly02@gmail.com>
2024-02-16 10:25:24 -0600

    Add modified RPS site

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function printScore(pScore, cScore) {
-  console.log(`Player Score: ${pScore}\nComputer Score: ${cScore}`)
+const form = document.querySelector("#buttonForm");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", getPlayerSelection());
+})
+
+function getPlayerSelection(event) {
+  const playerSelection = event.currentTarget.id;
+  
+  console.log(playerSelection);
 }
 
-function printOutcome(playerSelection, computerSelection, playerWon) {
-  if (playerWon) {
-    console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-  } else {
-    console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
-  }
-}
+// function printOutcome(playerSelection, computerSelection, playerWon) {
+//   if (playerWon) {
+//     console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
+//   } else {
+//     console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
+//   }
+// }
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
@@ -45,30 +54,28 @@ function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Choose [Rock], [Paper], [Scissors], or [E]xit: ");
+  // for (let i = 0; i < 5; i++) {
+  //   let playerChoice = prompt("Choose [Rock], [Paper], [Scissors], or [E]xit: ");
 
-    if (playerChoice == "E") {
-      break;
-    }
+  //   if (playerChoice == "E") {
+  //     break;
+  //   }
 
-    let computerChoice = getComputerChoice();
+  //   let computerChoice = getComputerChoice();
     
-    let wonRound = playRound(playerChoice, computerChoice);
+  //   let wonRound = playRound(playerChoice, computerChoice);
 
-    if (wonRound) {
-      playerScore++;
-      printOutcome(playerChoice, computerChoice, wonRound);
-    } else if (playerChoice == computerChoice) {
-      console.log("You tied with the Computer!")
-    } else if (!wonRound) {
-      computerScore++;
-      printOutcome(playerChoice, computerChoice, wonRound);
-    }
+  //   if (wonRound) {
+  //     playerScore++;
+  //     printOutcome(playerChoice, computerChoice, wonRound);
+  //   } else if (playerChoice == computerChoice) {
+  //     console.log("You tied with the Computer!")
+  //   } else if (!wonRound) {
+  //     computerScore++;
+  //     printOutcome(playerChoice, computerChoice, wonRound);
+  //   }
 
-    printScore(playerScore, computerScore);
+  //   printScore(playerScore, computerScore);
 
-  }
+  // }
 }
-
-playGame();
