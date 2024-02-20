cd16dbc905527ce65eb7079e43879bb4ccc8e44e
Nolan Reilly <nolanreilly02@gmail.com>
2024-02-19 20:31:12 -0600

    Add new UI features, and updating scores

--- index.js
+++ index.js
@@ -1,64 +1,12 @@
-// function printScore(pScore, cScore) {
-//   console.log(`Player Score: ${pScore}\nComputer Score: ${cScore}`)
-// }
+let playerScore = 0;
+let computerScore = 0;
 
-// function printOutcome(playerSelection, computerSelection, playerWon) {
-//   if (playerWon) {
-//     console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-//   } else {
-//     console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
-//   }
-// }
-
-// function playRound(playerSelection, computerSelection) {
-//   let wonRound = false;
-
-//   if (playerSelection == "Rock" && computerSelection == "Scissors") {
-//     wonRound = true;
-//   } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-//     wonRound = true;
-//   } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-//     wonRound = true;
-//   } else {
-//     wonRound = false;
-//   }
-
-//   return wonRound;
-// }
-
-// function playGame() {
-//   let playerScore = 0;
-//   let computerScore = 0;
-
-//   for (let i = 0; i < 5; i++) {
-//     let playerChoice = prompt("Choose [Rock], [Paper], [Scissors], or [E]xit: ");
-
-//     if (playerChoice == "E") {
-//       break;
-//     }
-
-//     let computerChoice = getComputerChoice();
-    
-//     let wonRound = playRound(playerChoice, computerChoice);
-
-//     if (wonRound) {
-//       playerScore++;
-//       printOutcome(playerChoice, computerChoice, wonRound);
-//     } else if (playerChoice == computerChoice) {
-//       console.log("You tied with the Computer!")
-//     } else if (!wonRound) {
-//       computerScore++;
-//       printOutcome(playerChoice, computerChoice, wonRound);
-//     }
-
-//     printScore(playerScore, computerScore);
-
-//   }
-// }
-
-// playGame();
 let playerSelection;
-let computerChoice;
+let computerSelection;
+
+let playerScoreText = document.querySelector("#playerScore");
+let computerScoreText = document.querySelector("#computerScore");
+let outcomeText = document.querySelector("#outcomeText");
 
 // Get all the buttons on the page
 const buttons = document.querySelectorAll("button");
@@ -77,13 +25,18 @@ buttons.forEach(function(button) {
     if (event.target.id == "scissors") {
       playerSelection = "Scissors";
     }
+
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+
+    playerScoreText.textContent = "Player Score: " + playerScore;
+    computerScoreText.textContent = "Computer Score: " + computerScore;
   });
 });
 
 // Gets the computers choice for each round that is played
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
-  let computerSelection = "";
 
   if (num == 0) {
     computerSelection = "Rock";
@@ -92,23 +45,24 @@ function getComputerChoice() {
   } else {
     computerSelection = "Scissors";
   }
-
-  return computerSelection;
 }
 
 // Play a round to determine if the player won or the computer won
 function playRound(playerSelection, computerSelection) {
-  let wonRound = false;
-
   if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    wonRound = true;
+    playerScore++;
+    outcomeText.textContent = "Rock beats Scissors";
   } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    wonRound = true;
+    playerScore++;
+    outcomeText.textContent = "Paper beats Rock";
   } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    wonRound = true;
+    playerScore++;
+    outcomeText.textContent = "Scissors beats Paper";
+  } else if (playerSelection == computerSelection) {
+    outcomeText.textContent = `Tied ${playerSelection} doesn't beat ${computerSelection}`
+    return;  // Exit function if the values are the same
   } else {
-    wonRound = false;
+    computerScore++;
+    outcomeText.textContent = `${computerSelection} beats ${playerSelection}`;
   }
-
-  return wonRound;
 }
